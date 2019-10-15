4ed8ddca910fb608c5bbb60ea74ad0a726450d57
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:43:42 +0200

    creazione di un applicazione con wpf

--- App/App-Multi-threading/MainWindow.xaml.cs
+++ App/App-Multi-threading/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace App_Multi_threading
 {
@@ -48,6 +50,29 @@ namespace App_Multi_threading
         private void AggiornaInterfaccia()
         {
             lbl_risultato.Content = "Finito";
+        }
+
+        private void btn_count_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(DoCount);
+        }
+
+        private void DoCount()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+
+                for (int j = 0; j < 100000; j++)
+                {
+                    Dispatcher.Invoke(()=>AggiornaInterfaccia(j));
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void AggiornaInterfaccia(int j)
+        {
+            lbl_conta.Content =j.ToString();
         }
     }
 }
