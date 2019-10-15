2db4ab795fd1a7ebda1821925101046119ed033f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 08:43:42 +0200

    crreazione wpf

---
+++ App/App-Multi-threading/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_Multi_threading
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
